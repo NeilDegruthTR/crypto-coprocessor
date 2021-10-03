a30246f69524658261132c9d3f6a422a789bf9a9
Yunus Emre ERYILMAZ <30428379+NeilDegruthTR@users.noreply.github.com>
2021-10-03 22:39:04 +0300

    Add files via upload

--- sim/tb_controller.v
+++ sim/tb_controller.v
@@ -3,8 +3,10 @@ module tb_controller ();
 reg clock = 0;
 reg [31:0] instruct = 0;
 wire [31:0] out;
+wire [4:0] sliceSelector;
+wire [5:0] writeEnableKey;
 
-controller dut (clock, instruct, out);
+controller dut (clock, instruct, out, sliceSelector, writeEnableKey);
 
 always begin
 #5;
@@ -13,8 +15,8 @@ clock = ~clock;
 end
 
 initial begin
-	/* instruct = 32'h80000000; //ef0bc156 ed8ff212 23f247b3 e0318a99
-	#45;
+	instruct = 32'h80000000; //ef0bc156 ed8ff212 23f247b3 e0318a99
+	#35;
 	instruct = 32'he0318a99;
 	#15;
 	instruct = 32'h23f247b3;
@@ -22,10 +24,10 @@ initial begin
 	instruct = 32'hed8ff212;
 	#20;
 	instruct = 32'hef0bc156;
-	#120; */
-	instruct = 32'h80000007;
+	#120;
+	/* instruct = 32'h80000007;
 	#30;
-	instruct = 32'h4;
+	instruct = 32'h4; */
 
 end
 endmodule
